6aae0cf4186c338ca4fe677aac165d145c1a11e6
Nano <Nano@DESKTOP-81CI3G2>
2024-05-17 18:37:48 -0600

    Agregar archivos de proyecto.

---
+++ SistemaMatricula/Form1.vb
@@ -0,0 +1,20 @@
+﻿Imports System.Data.OleDb
+
+Public Class Form1
+    Private Sub btnConexion_Click(sender As Object, e As EventArgs) Handles btnConexion.Click
+        Dim kaka As New OleDbConnection("Provider=Microsoft.Jet.Oledb.4.0; Data Source=F:\PROGRAMACION\SISTEMA MATRICULA\BASE DE DATOS\Matricula.mdb")
+        Try
+            kaka.Open()
+            MsgBox("Conectado")
+
+            Dim query = "Select * from ALUMNO"
+            Dim koko As New OleDbDataAdapter(query, kaka)
+            Dim vs As New DataTable
+            koko.Fill(vs)
+            DataGridView1.DataSource = vs
+        Catch ex As Exception
+            MsgBox("No se conecto por: " & ex.Message)
+
+        End Try
+    End Sub
+End Class
